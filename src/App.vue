f59e9ad423f722120e6f8b8c017f4d5febf74b94
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2020-04-07 19:02:38 -0300

    [FABOSSI] quote

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div v-if="quotes.length > 0" class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a quote to delete it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/newQuote.vue";
+
+export default {
+  data() {
+    return {
+      quotes: ["Just a quote"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (quote.length > 0) {
+        this.quotes.push(quote);
+      }
+      return;
+    },
+    deleteQuote(i) {
+      this.quotes.splice(i, 1);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(q, i) in quotes" :key="i" @click.native="deleteQuote(i)">{{ q }}</app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  },
+  methods: {
+    deleteQuote(i) {
+      this.$emit("quoteDeleted", i);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
